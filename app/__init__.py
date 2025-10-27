c25409b49ec3a63b4005a791c1a9b193220d0180
horaceo1b <horaceken0@gmail.com>
2025-10-27 11:38:49 +0300

    added patient route

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,30 @@
 from flask import Flask
 from .config import Config
-from .db import db,migrate
+from .db import db, migrate
 from .models import *
 from flask_bcrypt import Bcrypt
+
+from app.routes.patient import patient_bp
 
 bcrypt = Bcrypt()
 
 def create_app():
-    app=Flask(__name__)
+    app = Flask(__name__)
     app.config.from_object(Config)
 
-    # initialize db
+    # Initialize db and migrations
     db.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
 
-    # initialize bcrypt
+    # Initialize bcrypt
     bcrypt.init_app(app)
+
+    # ✅ Register Blueprints
+    app.register_blueprint(patient_bp)
+
+    @app.route("/")
+    def home():
+        return {"message": "MedBeta API is running"}
+    
 
     return app
